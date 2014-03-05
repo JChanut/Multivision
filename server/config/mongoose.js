55970069d5b1544ce3b4c21edf904ff432ee41bf
Jerry Chanut <jerry.chanut@steria.com>
2014-03-05 17:41:23 +0100

    Preparing for Login

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -7,4 +7,21 @@ module.exports = function(config) {
     db.once('open', function callback() {
         console.log('multivision db opened');
     });
+
+    var userSchema = mongoose.Schema({
+        firstName: String,
+        lastName: String,
+        username: String
+    });
+    var User = mongoose.model('User', userSchema);
+
+    User.find({}).exec(function (err, collection) {
+        if (collection.length === 0){
+            // Default User creation
+            User.create({firstName:'Jerry', lastName:'Chanut', username:'jerry'});
+            User.create({firstName:'Joe', lastName:'Eames', username:'joe'});
+            User.create({firstName:'John', lastName:'Papa', username:'john'});
+            User.create({firstName:'Dan', lastName:'Wahlin', username:'dan'});
+        }
+    });
 }
