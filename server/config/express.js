036d04d3bda859c2709a735de1b547d70ceae050
Jerry Chanut <jerry.chanut@steria.com>
2014-03-05 18:14:34 +0100

    Creating the Server Login Code

--- server/config/express.js
+++ server/config/express.js
@@ -10,7 +10,11 @@ module.exports = function (app, config) {
         app.set('views', config.rootPath + '/server/views');
         app.set('view engine', 'jade');
         app.use(express.logger('dev'));
+        app.use(express.cookieParser());
         app.use(express.bodyParser());
+        app.use(express.session({secret: 'multi vision unicorns'}));
+        app.use(passport.initialize());
+        app.use(passport.session());
         app.use(stylus.middleware({
             src: config.rootPath + '/public',
             compile: stylusCompile
