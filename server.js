650c1cdcc047244c85e5b28997e4501a91f1ad7d
Jerry Chanut <jerry.chanut@steria.com>
2014-03-05 10:13:16 +0100

    Add data in view

--- server.js
+++ server.js
@@ -32,13 +32,22 @@ db.once('open', function callback() {
     console.log('multivision db opened');
 });
 
+// Get data from mongodb
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function (err, messageDoc) {
+    mongoMessage = messageDoc.message;
+});
+
+
 app.get('/partials/:partialPath', function(req, res) {
     res.render('partials/' + req.params.partialPath);
 });
 
 
 app.get('*', function (req, res) {
-    res.render('index');
+    res.render('index', {'mongoMessage' : mongoMessage});
 });
 
 var port = 3000;
