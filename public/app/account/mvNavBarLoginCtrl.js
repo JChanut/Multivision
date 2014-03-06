827ef67f68d9c3d5830e57365fc4093bbd2870df
Jerry Chanut <jerry.chanut@steria.com>
2014-03-06 14:09:52 +0100

    Adding Signout Functionality

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, mvNotifier, mvIdentity, mvAuth) {
+angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, $location, mvNotifier, mvIdentity, mvAuth) {
 
     $scope.identity = mvIdentity;
 
@@ -10,6 +10,15 @@ angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, m
             else {
                 mvNotifier.notify('Username/Password combination incorrect');
             }
+        })
+    };
+
+    $scope.signout = function() {
+        mvAuth.logoutUser().then(function () {
+            $scope.username = "";
+            $scope.password = "";
+            mvNotifier.notify('You have successfully signed out!');
+            $location.path('/');
         })
     };
 });
