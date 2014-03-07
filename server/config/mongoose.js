e488897b37d7034a44d64d74bb42b89abbdb1b44
Jerry Chanut <jerry.chanut@steria.com>
2014-03-07 17:21:47 +0100

    Creating the Server-side Controller for user signin

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose'),
-    crypto = require('crypto');
+    encrypt = require('../utilities/encryption');
 
 module.exports = function(config) {
     mongoose.connect(config.db)
@@ -19,7 +19,7 @@ module.exports = function(config) {
     });
     userSchema.methods = {
         authenticate: function(passwordToMatch) {
-            return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+            return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
         }
     }
     var User = mongoose.model('User', userSchema);
@@ -28,27 +28,18 @@ module.exports = function(config) {
         if (collection.length === 0){
             // Default User creation
             var salt, hash;
-            salt = createSalt();
-            hash = hashPwd(salt, 'jerry');
+            salt = encrypt.createSalt();
+            hash = encrypt.hashPwd(salt, 'jerry');
             User.create({firstName:'Jerry', lastName:'Chanut', username:'jerry', salt: salt, hashed_pwd: hash, roles: ['admin']});
-            salt = createSalt();
-            hash = hashPwd(salt, 'joe');
+            salt = encrypt.createSalt();
+            hash = encrypt.hashPwd(salt, 'joe');
             User.create({firstName:'Joe', lastName:'Eames', username:'joe', salt: salt, hashed_pwd: hash, roles: ['admin']});
-            salt = createSalt();
-            hash = hashPwd(salt, 'john');
+            salt = encrypt.createSalt();
+            hash = encrypt.hashPwd(salt, 'john');
             User.create({firstName:'John', lastName:'Papa', username:'john', salt: salt, hashed_pwd: hash, roles: []});
-            salt = createSalt();
-            hash = hashPwd(salt, 'dan');
+            salt = encrypt.createSalt();
+            hash = encrypt.hashPwd(salt, 'dan');
             User.create({firstName:'Dan', lastName:'Wahlin', username:'dan', salt: salt, hashed_pwd: hash});
         }
     });
-}
-
-function createSalt() {
-    return crypto.randomBytes(128).toString('base64');
-};
-
-function hashPwd(salt, pwd) {
-    var hmac = crypto.createHmac('sha1', salt);
-    return hmac.update(pwd).digest('hex');
 }
