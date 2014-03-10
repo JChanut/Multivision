c33b053db3cb9f3120ada2ba59d6429eeca58452
Jerry Chanut <jerry.chanut@steria.com>
2014-03-10 17:22:38 +0100

    adding unit test for http ressource

--- test/tests/admin/mvUserListCtrlSpec.js
+++ test/tests/admin/mvUserListCtrlSpec.js
@@ -1,16 +1,36 @@
 describe('mvUserListCtrl', function () {
-    var ctrl;
+    var $httpBackend, mvUser, createController;
 
     beforeEach(function() {
         module('app');
-        inject(function ($controller, mvUser) {
-            ctrl = $controller('mvUserListCtrl', {mvUser: mvUser});
+        inject(function ($injector) {
+            $httpBackend = $injector.get('$httpBackend');
+            mvUser = $injector.get('mvUser');
+
+            // fake data
+            $httpBackend.when('GET', '/api/users').respond([
+                {firstName:'Jerry', lastName:'Chanut', username:'jerry', salt: '', hashed_pwd: '', roles: ['admin']},
+                {firstName:'Joe', lastName:'Eames', username:'joe', salt: '', hashed_pwd: '', roles: ['admin']}
+            ]);
+
+            var $controller = $injector.get('$controller');
+            createController = function() {
+                return $controller('mvUserListCtrl', {'mvUser': mvUser});
+            }
         });
     });
 
     describe('users', function () {
         it('should be an array', function () {
+            var ctrl = createController();
+            $httpBackend.flush();
             ctrl.users.should.be.a('Array');
+        });
+
+        it('should return all users', function() {
+            var ctrl = createController();
+            $httpBackend.flush();
+            ctrl.users.should.have.length(2);
         });
     });
 });
--- test/tests/main/mvMainCtrlSpec.js
+++ test/tests/main/mvMainCtrlSpec.js
@@ -1,20 +1,37 @@
 describe('mvMainCtrl', function () {
-    var ctrl;
+    var $httpBackend, mvCachedCourse, createController;
 
     beforeEach(function() {
         module('app');
-        inject(function ($controller) {
-            ctrl = $controller('mvMainCtrl');
+        inject(function ($injector) {
+            $httpBackend = $injector.get('$httpBackend');
+            mvCachedCourse = $injector.get('mvCachedCourse');
+
+            // Fake data
+            $httpBackend.when('GET', '/api/courses').respond([
+                    {title: 'C# for Sociopaths', featured: true, published: new Date('10/5/2013'), tags: ['C#']},
+                    {title: 'C# for Non-Sociopaths', featured: true, published: new Date('10/12/2013'), tags: ['C#']}
+                ]);
+
+            var $controller = $injector.get('$controller');
+
+            createController = function() {
+                return $controller('mvMainCtrl', {'mvCachedCourse': mvCachedCourse});
+            }
         });
     });
 
     describe('courses', function () {
         it('should be an array', function () {
+            var ctrl = createController();
+            $httpBackend.flush();
             ctrl.courses.should.be.a('Array');
         });
 
         it('should return all courses', function() {
-             ctrl.courses.should.have.length(15);
+            var ctrl = createController();
+            $httpBackend.flush();
+            ctrl.courses.should.have.length(2);
         });
     });
 });
