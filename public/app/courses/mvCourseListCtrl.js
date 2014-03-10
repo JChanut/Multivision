59f1f587cea9d8f73a4d650186428675ce284720
Jerry Chanut <jerry.chanut@steria.com>
2014-03-10 16:16:10 +0100

    Course List - Sorting, Filtering and Caching

--- public/app/courses/mvCourseListCtrl.js
+++ public/app/courses/mvCourseListCtrl.js
@@ -1,4 +1,11 @@
-angular.module('app').controller('mvCourseListCtrl', ['mvCourse', function (mvCourse) {
+angular.module('app').controller('mvCourseListCtrl', ['mvCachedCourse', function (mvCachedCourse) {
     var vm = this;
-    vm.courses = mvCourse.query();
+    vm.courses = mvCachedCourse.query();
+
+    vm.sortOptions = [
+        {value: 'title', text: 'Sort by title'},
+        {value: 'published', text: 'Sort by published data'}
+    ];
+
+    vm.sortOrder = vm.sortOptions[0].value;
 }]);
