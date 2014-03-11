c96ad60def0c3d6dfd63a6457e788492bb589c39
Jerry Chanut <jerry.chanut@steria.com>
2014-03-11 12:02:37 +0100

    Cleaning mvUserSpec unit test

--- test/tests/account/mvUserSpec.js
+++ test/tests/account/mvUserSpec.js
@@ -5,13 +5,13 @@ describe('mvUser', function () {
         it('should return false if the roles array does not have an admin entry', inject(function (mvUser) {
             var user = new mvUser();
             user.roles = ['not admin', 'simple user'];
-            expect(user.isAdmin()).to.be.false;
+            user.isAdmin().should.be.false;
         }));
 
         it('should return true if the roles array has an admin entry', inject(function (mvUser) {
             var user = new mvUser();
             user.roles = ['admin'];
-            expect(user.isAdmin()).to.be.true;
+            user.isAdmin().should.be.true;
         }));
     });
 });
