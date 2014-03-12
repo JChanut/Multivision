8f945d195543648e0ed3dad7215d41ec74a555e0
Jerry Chanut <jerry.chanut@steria.com>
2014-03-12 18:00:20 +0100

    adding gzip compression module

--- server/config/express.js
+++ server/config/express.js
@@ -3,17 +3,19 @@ var logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     session = require('express-session'),
     serveStatic = require('serve-static'),
+    compression = require('compression'),
     passport = require('passport'),
     stylus = require('stylus');
 
 module.exports = function (app, config) {
     function stylusCompile(str, path) {
-        return stylus(str).set('filename', path);
+        return stylus(str).set('filename', path).set('compress', true);
     }
 
     app.set('views', config.rootPath + '/server/views');
     app.set('view engine', 'jade');
     app.use(logger('dev')); // Logger middleware
+    app.use(compression());
     app.use(bodyParser());
     app.use(cookieParser());
     app.use(session({secret: 'multi vision unicorns'}));
@@ -23,6 +25,5 @@ module.exports = function (app, config) {
         src: config.rootPath + '/public',
         compile: stylusCompile
     }));
-
     app.use(serveStatic(config.rootPath + '/public'));
 };
