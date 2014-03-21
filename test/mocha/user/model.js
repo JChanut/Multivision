9c1aa1e6b1c85964090cc5fb07846b3bcebdbc8b
Jerry Chanut <jerry.chanut@steria.com>
2014-03-21 11:02:28 +0100

    Unit Test for server js files

--- test/mocha/user/model.js
+++ test/mocha/user/model.js
@@ -2,7 +2,7 @@ var should = require('chai').should(),
     User = require('mongoose').model('User');
 
 describe('User Model', function () {
-    var user;
+    var user, user2;
 
     beforeEach(function (done) {
         user = new User({
@@ -13,14 +13,28 @@ describe('User Model', function () {
             hashed_pwd: 'hash',
             roles: ['admin']
         });
+        user2 = new User(user);
 
         done();
     });
 
     describe('Method Save', function () {
+
+//        it('should begin with no users', function(done) {
+//            User.find({}, function(err, users) {
+//                users.should.have.length(0);
+//                done();
+//            });
+//        });
+
         it('should able to save without problem', function (done) {
-            return user.save(function (err) {
-                should.not.exist(err);
+            return user.save(done);
+        });
+
+        it('should fail to save an existing user again', function (done) {
+            user.save();
+            return user2.save(function (err) {
+                should.exist(err);
                 done();
             });
         });
@@ -43,6 +57,22 @@ describe('User Model', function () {
 
         it('should be able to show an error when try to save without username', function (done) {
             user.username = '';
+            return user.save(function (err) {
+                should.exist(err);
+                done();
+            });
+        });
+
+        it('should be able to show an error when try to save without salt', function (done) {
+            user.salt = '';
+            return user.save(function (err) {
+                should.exist(err);
+                done();
+            });
+        });
+
+        it('should be able to show an error when try to save without hashed password', function (done) {
+            user.hashed_pwd = '';
             return user.save(function (err) {
                 should.exist(err);
                 done();
