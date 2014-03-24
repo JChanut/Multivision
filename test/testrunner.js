b86115cb75a9d049bcdfa08b6778eff7c5674da9
Jerry Chanut <jerry.chanut@steria.com>
2014-03-24 10:27:15 +0100

    prepare test configuration for adding support of server test in travis.cy

---
+++ test/testrunner.js
@@ -0,0 +1,27 @@
+var karma = require('karma');
+
+karma.server.start(
+    {
+        // frameworks to use
+        frameworks: ['mocha', 'chai', 'sinon-chai'],
+        singleRun: true,
+        browsers: ['PhantomJS'],
+        // list of files / patterns to load in the browser
+        files: [
+            'public/vendor/underscore/underscore.js',
+            'public/vendor/angular/angular.js',
+            'public/vendor/angular-resource/angular-resource.js',
+            'public/vendor/angular-mocks/angular-mocks.js',
+            'test/karma/test-app.js',
+            'public/app/**/*.js',
+            'test/karma/**/*.js'
+        ],
+
+
+        // list of files to exclude
+        exclude: [
+            'public/app/app.js'
+        ]
+    }
+)
+//./node_modules/.bin/karma start --single-run --browsers PhantomJS
